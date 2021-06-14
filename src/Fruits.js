5e8d81f54a55fe7989dff5278a898b6beb2aee10
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-14 23:33:46 +0900

    Fruits.jsのHTML,scss完成

--- src/Fruits.js
+++ src/Fruits.js
@@ -1,5 +1,15 @@
+import "./Fruits.scss";
 import React from "react";
 
 export const Fruits = () => {
-  return <div>Helloworld!!</div>;
+  return (
+    <div className="review">
+      <div className="img-container">
+        <img src="" id="fruits-img" alt=""></img>
+        <h4 id="fruits"></h4>
+        <p id="color"></p>
+        <p id="info"></p>
+      </div>
+    </div>
+  );
 };
