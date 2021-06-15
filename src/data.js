2ad91913228435acb787f7b7cb817db1d3cd2098
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-15 15:50:21 +0900

    fruitsData.mapを定義

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const FruitsData = [
+export const fruitsData = [
   {
     id: 0,
     fruitsName: "Peach",
@@ -6,35 +6,4 @@ const FruitsData = [
     img: "./fruits/category_tn_190.jpeg",
     text: "山梨県で多く取れます",
   },
-
-  {
-    id: 1,
-    fruitsName: "Cherry",
-    color: "Red",
-    img: "./fruits/category_tn_192.jpeg",
-    text: "山形県で多く取れます",
-  },
-  {
-    id: 2,
-    fruitsName: "Apple",
-    color: "Red",
-    img: "./fruits/category_tn_193.jpeg",
-    text: "青森県で多く取れます",
-  },
-  {
-    id: 3,
-    fruitsName: "Grape",
-    color: "Purple",
-    img: "./fruits/category_tn_195.jpeg",
-    text: "山梨で多く取れます",
-  },
-  {
-    id: 4,
-    fruitsName: "Banana",
-    color: "Yellow",
-    img: "./fruits/category_tn_197.jpeg",
-    text: "朝食にぴったりです",
-  },
 ];
-
-export default FruitsData;
