ca214433e4495d78869b304586e545c65827497e
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-15 17:16:53 +0900

    修正

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const fruitsData = [
     id: 0,
     fruitsName: "Peach",
     color: "Pink",
-    img: "./fruits/category_tn_190.jpeg",
+    img: "https://food-foto.jp/img/category_tn_190.jpg",
     text: "山梨県で多く取れます",
   },
 ];
