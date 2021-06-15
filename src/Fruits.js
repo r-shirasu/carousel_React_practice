717eebb70441295903ae1e95899291ca3548d102
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-15 16:50:33 +0900

    0番目のデータを表示

--- src/Fruits.js
+++ src/Fruits.js
@@ -4,19 +4,19 @@ import { fruitsData } from "./data";
 
 export const Fruits = () => {
   return (
-    <div className="review">
+    <>
       {fruitsData.map((fruitsarray, fruitsIndex) => {
-        fruitsIndex = 0;
-
-        <div className="img-container" key={fruitsarray.id}>
-          <img src="" id="img" alt="">
-            {fruitsarray.img}
-          </img>
-          <h4 id="fruits">{fruitsarray.fruitsName}</h4>
-          <p id="color">{fruitsarray.color}</p>
-          <p id="info">{fruitsarray.text}</p>
-        </div>;
+        return (
+          <div className="img-container" key={fruitsarray.id}>
+            <img src={fruitsarray.img} id="img" alt=""></img>
+            <h4 id="fruits">{fruitsarray.fruitsName}</h4>
+            <p id="color">{fruitsarray.color}</p>
+            <p id="info">{fruitsarray.text}</p>
+          </div>
+        );
       })}
-    </div>
+    </>
   );
 };
+
+// F9gZemJQ;
