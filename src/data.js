2738b1200cfe8a0fc235a016ee911f6e8ffcf859
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-15 17:31:52 +0900

    データ追加

--- src/data.js
+++ src/data.js
@@ -39,4 +39,32 @@ export const fruitsData = [
     text: "朝食にぴったりです",
     alt: "Banana",
   },
+  {
+    id: 1,
+    fruitsName: "Cherry",
+    color: "Red",
+    img: "./fruits/category_tn_192.jpeg",
+    text: "山形県で多く取れます",
+  },
+  {
+    id: 2,
+    fruitsName: "Apple",
+    color: "Red",
+    img: "./fruits/category_tn_193.jpeg",
+    text: "青森県で多く取れます",
+  },
+  {
+    id: 3,
+    fruitsName: "Grape",
+    color: "Purple",
+    img: "./fruits/category_tn_195.jpeg",
+    text: "山梨で多く取れます",
+  },
+  {
+    id: 4,
+    fruitsName: "Banana",
+    color: "Yellow",
+    img: "./fruits/category_tn_197.jpeg",
+    text: "朝食にぴったりです",
+  },
 ];
