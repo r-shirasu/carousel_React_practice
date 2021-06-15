378c0a9f0d6bbda01bebc109d1ae170c15ebcea2
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-15 15:50:21 +0900

    fruitsData.mapを定義

--- src/Fruits.js
+++ src/Fruits.js
@@ -1,15 +1,22 @@
 import "./Fruits.scss";
 import React from "react";
+import { fruitsData } from "./data";
 
 export const Fruits = () => {
   return (
     <div className="review">
-      <div className="img-container">
-        <img src="" id="fruits-img" alt=""></img>
-        <h4 id="fruits"></h4>
-        <p id="color"></p>
-        <p id="info"></p>
-      </div>
+      {fruitsData.map((fruitsarray, fruitsIndex) => {
+        fruitsIndex = 0;
+
+        <div className="img-container" key={fruitsarray.id}>
+          <img src="" id="img" alt="">
+            {fruitsarray.img}
+          </img>
+          <h4 id="fruits">{fruitsarray.fruitsName}</h4>
+          <p id="color">{fruitsarray.color}</p>
+          <p id="info">{fruitsarray.text}</p>
+        </div>;
+      })}
     </div>
   );
 };
