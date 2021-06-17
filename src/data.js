80939c9f0c319a7a29b250e5a88e4b786b348678
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-17 11:48:27 +0900

    altの記述修正

--- src/data.js
+++ src/data.js
@@ -39,32 +39,4 @@ export const fruitsData = [
     text: "朝食にぴったりです",
     alt: "Banana",
   },
-  {
-    id: 1,
-    fruitsName: "Cherry",
-    color: "Red",
-    img: `${process.env.PUBLIC_URL}/fruits/category_tn_192.jpeg`,
-    text: "山形県で多く取れます",
-  },
-  {
-    id: 2,
-    fruitsName: "Apple",
-    color: "Red",
-    img: `${process.env.PUBLIC_URL}/fruits/category_tn_193.jpeg`,
-    text: "青森県で多く取れます",
-  },
-  {
-    id: 3,
-    fruitsName: "Grape",
-    color: "Purple",
-    img: `${process.env.PUBLIC_URL}/fruits/category_tn_195.jpeg`,
-    text: "山梨で多く取れます",
-  },
-  {
-    id: 4,
-    fruitsName: "Banana",
-    color: "Yellow",
-    img: `${process.env.PUBLIC_URL}/fruits/category_tn_197.jpeg`,
-    text: "朝食にぴったりです",
-  },
 ];
